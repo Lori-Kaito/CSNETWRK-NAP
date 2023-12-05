3c33d2ce1c64eac6a2920c92bdd943cf6db811ee
RK <delmundork@gmail.com>
2023-12-05 19:28:59 +0800

    new codes

--- FileExchangeClient.java
+++ FileExchangeClient.java
@@ -1,43 +1,29 @@
 import java.io.*;
 import java.net.*;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Scanner;
 
 public class FileExchangeClient {
-    private static final int BUFFER_SIZE = 1024; //buffer size
+    private static final int BUFFER_SIZE = 1024;
 
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter server IP address and port (e.g., /join 127.0.0.1 12345): "); 
-            String joinCommand = scanner.nextLine(); //user will input the server IP address and port
-
+            System.out.print("Enter server IP address and port (e.g., /join 127.0.0.1 8080): ");
+            String joinCommand = scanner.nextLine();
+            
             String[] joinTokens = joinCommand.split("\\s+");
-            if (joinTokens.length == 3 && joinTokens[0].equalsIgnoreCase("/join")) { //condition to check if valid in using /join
+            if (joinTokens.length == 3 && joinTokens[0].equalsIgnoreCase("/join")) {
                 String serverIP = joinTokens[1];
                 int serverPort = Integer.parseInt(joinTokens[2]);
 
                 Socket socket = new Socket(serverIP, serverPort);
                 BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
-                
-                command:
+
                 while (true) {
-                    System.out.print("Enter command: "); //enters what command
+                    System.out.print("Enter command: ");
                     String command = scanner.nextLine();
                     writer.println(command);
-
-                    if (command.toLowerCase().startsWith("/store")) {
-                        String[] storeTokens = command.split("\\s+");
-                        if (storeTokens.length == 2) {
-                            String filePath = storeTokens[1];
-                            sendFile(socket, filePath);
-                        } else {
-                            System.out.println("Error: Invalid parameters for /store command.");
-                            break command;
-                        }
-                    }
 
                     String response = reader.readLine();
                     System.out.println("Server: " + response);
@@ -49,37 +35,10 @@ public class FileExchangeClient {
 
                 socket.close();
             } else {
-                System.out.println("Invalid join command. Please restart the client and provide a valid /join command."); //disconnects the client
+                System.out.println("Invalid join command. Please restart the client and provide a valid /join command.");
             }
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
-        }
-    }
-
-    private static void sendFile(Socket socket, String filePath) throws IOException {
-        BufferedOutputStream fileOutputStream = new BufferedOutputStream(socket.getOutputStream());
-        FileInputStream fileInputStream = new FileInputStream(filePath);
-    
-        try {
-            byte[] buffer = new byte[BUFFER_SIZE];
-            int bytesRead;
-    
-            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                fileOutputStream.write(buffer, 0, bytesRead);
-            }
-    
-            // Get the current timestamp
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            String timestamp = dateFormat.format(new Date());
-    
-            // Extract user handle from the /register command
-            String userHandle = "User1"; // Update this with the actual user handle
-            System.out.println(userHandle + "<" + timestamp + ">: Uploaded " + new File(filePath).getName());
-        } finally {
-            fileOutputStream.flush();
-            fileInputStream.close();
-            // You might not want to close the socket here if you're using it for other purposes
-            // socket.close();
         }
     }
 }
--- FileExchangeServer.java
+++ FileExchangeServer.java
@@ -1,73 +1,51 @@
 import java.io.*;
 import java.net.*;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 public class FileExchangeServer {
-    private static final int PORT = 12345; // the server port
-    private static final int BUFFER_SIZE = 1024; // buffer size
+    private static final int PORT = 8080; // Server port
+    private static final int BUFFER_SIZE = 1024;
     private static Set<String> registeredHandles = new HashSet<>();
 
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(PORT); // port of the server
+            ServerSocket serverSocket = new ServerSocket(PORT);
             System.out.println("Server is running on port " + PORT);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected: " + clientSocket.getInetAddress().getHostAddress());
 
-                // Handle each client in a separate thread
-                Thread clientThread = new Thread(() -> handleClient(clientSocket));
-                clientThread.start();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                String command;
+                while ((command = reader.readLine()) != null) {
+                    String response = processCommand(command);
+                    writer.println(response);
+
+                    if (command.equalsIgnoreCase("/leave")) {
+                        System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
+                        break;
+                    }
+                }
+
+                clientSocket.close();
             }
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
 
-    private static void handleClient(Socket clientSocket) {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
-    
-            String command;
-            while ((command = reader.readLine()) != null) {
-                if (clientSocket.isClosed()) {
-                    // Check if the socket is closed
-                    System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
-                    break;
-                }
-                
-                String response = processCommand(command, clientSocket);
-                if (response == null) {
-                    // If response is null, the socket might have been closed
-                    System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
-                    break;
-                }
-                writer.println(response);
-    
-                if (command.equalsIgnoreCase("/leave")) {
-                    System.out.println("Client disconnected: " + clientSocket.getInetAddress().getHostAddress());
-                    break;
-                }
-            }
-    
-            clientSocket.close();
-        } catch (IOException e) {
-            System.err.println("Error handling client: " + e.getMessage());
-        }
-    }
-
-    private static String processCommand(String command, Socket clientSocket) {
+    private static String processCommand(String command) {
         String[] tokens = command.split("\\s+");
         String action = tokens[0].toLowerCase();
-    
+
         switch (action) {
             case "/dir":
-                // shows the files in the server directory
+                // Logic to list files in the server directory
                 File serverDirectory = new File("server_directory");
                 File[] files = serverDirectory.listFiles();
                 StringBuilder fileList = new StringBuilder("Server Directory:\n");
@@ -77,9 +55,9 @@ public class FileExchangeServer {
                     }
                 }
                 return fileList.toString();
-    
+
             case "/get":
-                // used to get the specific file that the user wants
+                // Logic to fetch a file from the server
                 if (tokens.length == 2) {
                     String fileName = tokens[1];
                     File requestedFile = new File("server_directory", fileName);
@@ -91,66 +69,27 @@ public class FileExchangeServer {
                 } else {
                     return "Error: Invalid parameters for /get command.";
                 }
-    
-            case "/leave":
-                return "Connection closed. Thank you!"; //if client wants to leave in the server
 
-            case "/register": //command to register handle or alias from the client
-                return registerHandle(tokens);
+            case "/leave":
+                return "Connection closed. Thank you!";
+
+            case "/register":
+                if (tokens.length == 2) {
+                    String handle = tokens[1];
+                    if (!registeredHandles.contains(handle)) {
+                        registeredHandles.add(handle);
+                        return "Welcome " + handle + "!";
+                    } else {
+                        return "Error: Handle or alias already exists.";
+                    }
+                } else {
+                    return "Error: Invalid parameters for /register command.";
+                }
 
             // Add logic for other commands (/store, etc.) here
 
-            case "/store":
-                try {
-                    receiveFile(clientSocket, command);
-                    return "File stored successfully."; // Send a success message
-                } catch (IOException e) {
-                    return "Error storing file: " + e.getMessage(); // Send an error message
-                }
-
             default:
                 return "Error: Command not found.";
-        }
-    }
-    
-    
-
-
-    private static String registerHandle(String[] tokens) {
-        if (tokens.length == 2) {
-            String handle = tokens[1];
-            if (!registeredHandles.contains(handle)) {
-                registeredHandles.add(handle);
-                return "Welcome " + handle + "!";
-            } else {
-                return "Error: Handle or alias already exists.";
-            }
-        } else {
-            return "Error: Invalid parameters for /register command.";
-        }
-    }
-
-    private static void receiveFile(Socket socket, String command) throws IOException {
-        String[] storeTokens = command.split("\\s+");
-        if (storeTokens.length == 2) {
-            String fileName = storeTokens[1];
-            try (BufferedInputStream fileInputStream = new BufferedInputStream(socket.getInputStream());
-                 FileOutputStream fileOutputStream = new FileOutputStream("server_directory/" + fileName)) {
-    
-                byte[] buffer = new byte[BUFFER_SIZE];
-                int bytesRead;
-    
-                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
-                    fileOutputStream.write(buffer, 0, bytesRead);
-                }
-    
-                // Get the current timestamp
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                String timestamp = dateFormat.format(new Date());
-    
-            }
-        } else {
-            throw new IOException("Invalid parameters for /store command.");
         }
     }
 }
